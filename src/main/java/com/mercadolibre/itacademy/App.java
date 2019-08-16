b2e3fecae724fb67db950313afe57b3212a8c1df
Facu33 <miradord18>
2019-08-16 15:08:25 -0300

    fixing path

--- src/main/java/com/mercadolibre/itacademy/App.java
+++ src/main/java/com/mercadolibre/itacademy/App.java
@@ -12,17 +12,17 @@ public class App extends Jooby {
         use(new Jackson());
         UserController userController = new UserController();
 
-        post("/items/login", (req, res) -> userController.doLogin(req, res));
+        post("items/login", (req, res) -> userController.doLogin(req, res));
 
-        get("/items/sites", (req, res) -> userController.getSites(req, res));
+        get("items/sites", (req, res) -> userController.getSites(req, res));
 
-        get("/items/sites/:id/categories", (req, res) -> userController.getSitesCategories(req, res));
+        get("items/sites/:id/categories", (req, res) -> userController.getSitesCategories(req, res));
 
-        post("/items/item", (req, res) -> userController.postItem(req, res));
+        post("items/item", (req, res) -> userController.postItem(req, res));
 
-        get("/items/item", (req, res) -> userController.getItems(req, res));
+        get("items/item", (req, res) -> userController.getItems(req, res));
 
-        get("/items/item/:username", (req, res) -> userController.getItemsxUser(req, res));
+        get("items/item/:username", (req, res) -> userController.getItemsxUser(req, res));
 
     }
 
