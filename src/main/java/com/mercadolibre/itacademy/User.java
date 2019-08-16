fa6510ae54cdaa3fb9b51f9dd30c709f62e2b1b0
Facu33 <miradord18>
2019-08-15 22:51:19 -0300

    adding get sites endpoint

--- src/main/java/com/mercadolibre/itacademy/User.java
+++ src/main/java/com/mercadolibre/itacademy/User.java
@@ -3,17 +3,27 @@ package com.mercadolibre.itacademy;
 public class User {
     public String username;
     public String password;
+    public String token;
+
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "name='" + username + '\'' +
-                "password='" + password + '\'' +
+                "token='" + token + '\'' +
                 '}';
     }
 }
--- src/main/java/com/mercadolibre/itacademy/UserService.java
+++ src/main/java/com/mercadolibre/itacademy/UserService.java
@@ -3,25 +3,48 @@ package com.mercadolibre.itacademy;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
+import java.net.*;
 
 public class UserService {
 
-    public String get(String url, User user) throws Exception {
-        return "";
+    public String get(String strUrl,String username,String token) throws Exception {
+
+        StringBuilder stringBuilder = new StringBuilder(strUrl);
+        stringBuilder.append("?username=");
+        stringBuilder.append(URLEncoder.encode(username, "UTF-8"));
+        stringBuilder.append("&token=");
+        stringBuilder.append(URLEncoder.encode(token, "UTF-8"));
+
+        URL url = new URL(stringBuilder.toString());
+
+        URLConnection con = url.openConnection();
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        if (con instanceof HttpURLConnection) {
+            HttpURLConnection connection = (HttpURLConnection) con;
+            System.out.println("\nSending request to URL : " + url);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuffer response = new StringBuffer();
+
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            in.close();
+
+            return response.toString();
+        } else {
+            System.out.println("URL invalida");
+            return null;
+        }
     }
 
 
-    public static String post(String urlGet, User user) {
+    public static String postLogin(String urlGet, User user) {
         try {
             URL url = new URL(urlGet);
-
             try {
                 HttpURLConnection con = (HttpURLConnection) url.openConnection();
                 con.setDoOutput(true);
